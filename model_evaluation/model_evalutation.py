97e71b8555067b496898e6efd0bdf8915962af24
tommyEzreal <100064247+tommyEzreal@users.noreply.github.com>
2022-12-26 16:56:57 +0900

    Update model_evalutation.py

--- model_evaluation/model_evalutation.py
+++ model_evaluation/model_evalutation.py
@@ -8,9 +8,11 @@ from tqdm.auto import tqdm
 
 
 
-def model_evaluation(model_save_path,test_data_path):
+def model_evaluation(model_save_path):
     
     cross_encoder = CrossEncoder(model_save_path)
+    
+    test_data_path = 'https://raw.githubusercontent.com/tommyEzreal/SolProject3-STS-Bechmark-improvement/main/data/new_devset.csv'
     test = pd.read_csv(test_data_path)
     test = test.rename(columns={'new_label':'labels.label'})
     test = test[['sentence1','sentence2','labels.label']]
